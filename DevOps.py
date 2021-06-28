4f4603404319ee2c55fbbfbebcd8b28bd42195c7
“DefiantMalice” <defiantmalice@gmail.com>
2021-06-27 22:58:22 -0700

    First Day of Python

---
+++ DevOps.py
@@ -0,0 +1,3 @@
+n = 20
+if n == 20:
+    print('The numbers is 20')
--- while_loop_with_break.py
+++ while_loop_with_break.py
@@ -2,5 +2,5 @@ while True:
     string = input(' Enter some text to print. \nType "done" to quit > ')
     if string == 'done':
         break
-        print(string)
+    print(string)
 print(' Done!')
